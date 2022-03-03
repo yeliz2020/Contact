7c70d4df05b5952895f0355286381cda25829ebd
Yeliz İşler <yeliz.isler@gmail.com>
2022-03-03 15:48:18 +0300

    add components

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Form() {
+  const [form, setForm] = useState({ fullname: "", phone_number: "" });
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+    console.log(form);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <input
+          name="phone_number"
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
